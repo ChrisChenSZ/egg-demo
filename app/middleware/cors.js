9e8a876d4279bcd42e94cca3c214652bbd36a75d
chenyuntao <88316842@qq.com>
2019-01-22 21:00:15 +0800

    # 增加跨域

--- app/middleware/cors.js
+++ app/middleware/cors.js
@@ -2,20 +2,24 @@
 
 module.exports = () => {
   return async function(ctx, next) {
-    console.log(ctx);
-    ctx.set('Access-Control-Allow-Origin', '*');
-    // ctx.set(
-    //   'Access-Control-Allow-Origin',
-    //   'http://192.168.16.105:7001,http://127.0.0.1:7001'
-    // );
-    ctx.set(
-      'Access-Control-Allow-Methods',
-      'GET, POST, DELETE, PUT, PATCH, OPTIONS'
-    );
-    ctx.set(
-      'Access-Control-Allow-Headers',
-      'Content-Type, api_key, Authorization'
-    );
+    // ctx.set('Access-Control-Allow-Origin', '*');
+    const { request } = ctx;
+    console.log(request.header.origin);
+    if (
+      request.header.origin === 'http://192.168.16.105:7001' ||
+      request.header.origin === 'http://127.0.0.1:7001'
+    ) {
+      ctx.set('Access-Control-Allow-Origin', request.header.origin);
+      ctx.set(
+        'Access-Control-Allow-Methods',
+        'GET, POST, DELETE, PUT, PATCH, OPTIONS'
+      );
+      ctx.set(
+        'Access-Control-Allow-Headers',
+        'Content-Type, api_key, Authorization'
+      );
+    }
+
     next();
   };
 };
